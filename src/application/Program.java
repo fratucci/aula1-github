fae930dc5c528c78fe251a1d43de615392072de0
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2019-12-05 15:12:36 -0300

    Adicionada msensagem de boa tarde

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,9 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Bum dia");
+		System.out.println("Bom dia");
+		System.out.println("Boa tarde");
+		
 	}
 
 }
